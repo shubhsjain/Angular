b9ff3687fb840d6898d773d3bd143b7abf5d2f36
shubhsjain <svj8866@gmail.com>
2020-12-28 13:23:31 +0530

    interpoletion/ property binding/ on click/ if else statement

---
+++ src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  title = '';
+
+    // event
+  
+  // currentval = ""
+
+  // getval(val: any) {
+  //   console.log('val')
+  //   this.currentval = val
+  // }
+
+  //  interpoletion vs property binding
+
+  // name = "Shubh"
+
+  // disabledBox = true
+
+  // enableBox() {
+  //   this.disabledBox = false
+  // }
+
+  // if else conditions
+
+  // show = true
+
+  // show = "yellow"
+
+
+}
